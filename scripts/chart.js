2b9616858a03e4bd192c2cca5ff380073fc5c4da
Ricky Chon <rickychon99@gmail.com>
2021-02-28 04:40:26 -0500

    Add completed fruit bar chart

---
+++ scripts/chart.js
@@ -0,0 +1,39 @@
+let fruits = [
+    { name: "Apple", quantity: 20, color: "red" },
+    { name: "Orange", quantity: 10, color: "orange" },
+    { name: "Banana", quantity: 15, color: "yellow" },
+    { name: "Kiwi", quantity: 3, color: "green" },
+    { name: "Blueberry", quantity: 5, color: "blue" },
+    { name: "Grapes", quantity: 8, color: "purple" }
+];
+
+const canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+function init()
+{
+    window.requestAnimationFrame(draw);
+}
+
+function draw()
+{
+    window.requestAnimationFrame(draw);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+
+    fruits.forEach((fruit, index) =>
+    {
+        // Bar
+        ctx.fillStyle = fruit.color;
+        ctx.fillRect(index * 100, canvas.height - (fruit.quantity * 30), canvas.width / fruits.length, canvas.height);
+
+        // Caption
+        ctx.fillStyle = 'black';
+        ctx.font = 'bold 24px serif';
+        ctx.textAlign = 'center';
+        ctx.fillText(fruit.quantity, (index * 100) + 50, canvas.height - 50, canvas.width / fruits.length);
+        ctx.fillText(fruit.name, (index * 100) + 50, canvas.height - 25, canvas.width / fruits.length);
+    });
+}
+
+init();
